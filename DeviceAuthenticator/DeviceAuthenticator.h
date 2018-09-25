ca2003fbbf183b7ad8aa2648eefb18c1126078e3
eMdOS <emilio.ojeda.mendoza@gmail.com>
2018-09-25 18:06:26 -0500

    chore: initial commit

---
+++ DeviceAuthenticator/DeviceAuthenticator.h
@@ -0,0 +1,10 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceAuthenticator.
+FOUNDATION_EXPORT double DeviceAuthenticatorVersionNumber;
+
+//! Project version string for DeviceAuthenticator.
+FOUNDATION_EXPORT const unsigned char DeviceAuthenticatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceAuthenticator/PublicHeader.h>
---
+++ DeviceAuthenticator/sources/BiometricType.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+public enum BiometricType: String {
+    case none = "None"
+    case touchID = "Touch ID"
+    case faceID = "Face ID"
+}
