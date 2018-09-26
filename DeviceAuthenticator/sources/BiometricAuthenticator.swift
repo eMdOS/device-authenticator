ad8dd332d3b688f8c72a1d4e46cf10a5a6015d00
eMdOS <emilio.ojeda.mendoza@gmail.com>
2018-09-25 19:05:21 -0500

    chore: add `isAvailable` property and `authenticate` function

--- DeviceAuthenticator/sources/BiometricAuthenticator.swift
+++ DeviceAuthenticator/sources/BiometricAuthenticator.swift
@@ -19,11 +19,29 @@ public class BiometricAuthenticator {
 }
 
 extension BiometricAuthenticator: BiometricAuthenticatorType {
+    public var isAvailable: Bool {
+        return biometricType != .none
+    }
+
     public var biometricType: BiometricType {
         switch authenticationContext.biometryType {
         case .none: return .none
         case .touchID: return .touchID
         case .faceID: return .faceID
+        }
+    }
+
+    public func authenticate(onSuccess: (() -> Void)?, onError: ((Error) -> Void)?) {
+        if canEvaluatePolicy {
+            authenticationContext.evaluatePolicy(policy, localizedReason: "Localized Reason") { (isSuccess, error) in
+                if let error = error {
+                    onError?(error)
+                } else if isSuccess {
+                    onSuccess?()
+                }
+            }
+        } else if let error = authenticationError {
+            onError?(error)
         }
     }
 }
--- DeviceAuthenticator/sources/BiometricAuthenticatorType.swift
+++ DeviceAuthenticator/sources/BiometricAuthenticatorType.swift
@@ -2,5 +2,7 @@
 import Foundation
 
 public protocol BiometricAuthenticatorType: class {
+    var isAvailable: Bool { get }
     var biometricType: BiometricType { get }
+    func authenticate(onSuccess: (() -> Void)?, onError: ((Swift.Error) -> Void)?)
 }
